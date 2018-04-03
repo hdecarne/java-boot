e9efb0890fe281fd7c573cc9b85ab89d48df3892
Holger <hdecarne@gmail.com>
2018-04-03 08:14:57 +0200

    Move logging from default into boot (as it is also bootstrapped)

---
+++ src/test/java/de/carne/boot/test/logging/LocalizedFilterTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016-2018 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.boot.test.logging;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import de.carne.boot.logging.LocalizedFilter;
+import de.carne.boot.logging.Log;
+import de.carne.boot.logging.LogBuffer;
+import de.carne.boot.logging.Logs;
+
+/**
+ * Test {@linkplain LocalizedFilter} class.
+ */
+class LocalizedFilterTest {
+
+	@Test
+	void testLocalizedFilter() throws IOException {
+		Logs.readConfig("logging-localized.properties");
+
+		Log standardLog = new Log(LogTest.class);
+		Log localizedLog1 = new Log(getClass().getName());
+		Log localizedLog2 = new Log(LogsTest.class, getClass().getName());
+		LogRecordCounter counter = new LogRecordCounter();
+
+		LogBuffer.addHandler(standardLog, counter, true);
+		LoggingTests.logTestMessages(localizedLog1, 6);
+
+		Assertions.assertEquals(6, counter.getPublishCount());
+
+		LoggingTests.logTestMessages(localizedLog2, 6);
+
+		Assertions.assertEquals(12, counter.getPublishCount());
+
+		LoggingTests.logTestMessages(standardLog, 6);
+
+		Assertions.assertEquals(12, counter.getPublishCount());
+	}
+
+}
